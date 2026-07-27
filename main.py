2e8983189a5b34bc00195db8138a144b32602322
Amit Kumar <amitkumar8178552618@gmail.com>
2026-07-27 15:19:47 +0530

    Data Ingestion Complete

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from src.cnnclassifier import logger
+from cnnclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline, STAGE_NAME
 
+
+
+if __name__== '__main__':
+    try:
+        logger.info(f">>>>>>>>{STAGE_NAME}<<<<<<<<<")  
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f'>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<')
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
 
